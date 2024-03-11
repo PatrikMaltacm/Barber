1e7c938211e13915cda9c815fdc2a40cb2939f0c
Patrik Malta <maltapatrik3@gmail.com>
2024-03-11 17:14:41 -0400

    first deploy

---
+++ ts-react/src/Map.tsx
@@ -0,0 +1,8 @@
+const ref = React.useRef<HTMLDivElement>(null);
+const [map, setMap] = React.useState<google.maps.Map>();
+
+React.useEffect(() => {
+  if (ref.current && !map) {
+    setMap(new window.google.maps.Map(ref.current, {}));
+  }
+}, [ref, map]);
